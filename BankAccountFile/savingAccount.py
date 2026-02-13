d6ec4baf2cd18be50d6e07fb0619af7c80815d52
Deeraj Anandraj <deeraj0830@gmail.com>
2026-02-13 13:59:53 -0500

    made child class and made first object

--- BankAccountFile/savingAccount.py
+++ BankAccountFile/savingAccount.py
@@ -1,1 +1,5 @@
 from BankAccountFile.BankAccountPart2 import BankAccount
+
+class SavingAccount(BankAccount):
+    def __init__(self, customer_name, current_balance, minimum_balance, interest_rate):
+        super().__init__(customer_name, current_balance, minimum_balance)
