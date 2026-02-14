3fbbccf57d23b36e109893a5d3a352250634adf0
bchen18-charlotte <bchen18@charlotte.edu>
2026-02-13 20:12:11 -0500

    added transfer method with limit and validation on transfer

--- BankAccountFile/checkingAccount.py
+++ BankAccountFile/checkingAccount.py
@@ -7,3 +7,16 @@ class CheckingAccount(BankAccount):
         super().__init__(customer_name, current_balance, minimum_balance, account_number, routing_number)
 
         self.transfer_limit = transfer_limit
+    
+    def transfer(self, amount):
+       
+        if amount > self.transfer_limit:
+            print("Transfer exceeds limit")
+
+
+        elif self.current_balance - amount >= self.minimum_balance:
+            self.current_balance -= amount
+            print("Transfer successful")
+       
+        else:
+            print("Not enough money")
